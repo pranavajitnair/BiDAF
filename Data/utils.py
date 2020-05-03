02a3e66d0df337ea970456f21324e66c1ddcc158
pranavajitnair <pranavajitnair.cse18@itbhu.ac.in>
2020-05-03 19:42:09 +0530

    evaluation scores and predictor added

--- Data/utils.py
+++ Data/utils.py
@@ -2,8 +2,10 @@ import json
 import string
 import gensim.models as gs
 
+import torch.nn.functional as F
+
 def read_data(path):
-#        '/home/pranav/ml/data/SQuAD 1.1/train-v1.1.json'
+
         with open(path) as f:
                 file=json.load(f)['data']
                 
@@ -11,6 +13,16 @@ def read_data(path):
             
 def get_structure(file):
         char_set=set()
+        char_set.add('̇')
+        char_set.add('ꮉ')
+        char_set.add('լ')
+        char_set.add('ⲭ')
+        char_set.add('պ')
+        char_set.add('ј')
+        char_set.add('є')
+        char_set.add('ἥ')
+
+
         puncts=list(string.punctuation)
         output={'qids':[],'questions':[],'answers':[],'contexts':[]}
     
@@ -96,15 +108,19 @@ def get_index(output,char_set):
                 for word in contexts:
                        ma=max(ma,len(word))
                        
-                for word in context:
-                        l=[]
+                for word in contexts:
+                        o=[]
                         
                         for character in word:
-                                l.append(char_to_int[character])
-                        for _ in range(ma-len(word)):
-                                l.append(char_to_int['PAD'])
+                                try:
+                                    o.append(char_to_int[character])
+                                except:
+                                    aa=1
+
+                        for _ in range(ma-len(o)):
+                                o.append(char_to_int['PAD'])
                             
-                        context.append(l)
+                        context.append(o)
                         
                 final_output['contexts'].append(context)
                 
@@ -119,20 +135,20 @@ def get_index(output,char_set):
                                 ma=max(ma,len(word))
                                 
                         for word in question:
-                                l=[]
+                                o=[]
                                 
                                 for character in word:
-                                        l.append(char_to_int[character])
-                                for _ in range(ma-len(word)):
-                                        l.append(char_to_int['PAD'])
+                                        o.append(char_to_int[character])
+                                for _ in range(ma-len(o)):
+                                        o.append(char_to_int['PAD'])
                                         
-                                x.append(l)
+                                x.append(o)
      
                         sets.append(x)
                         
                 final_output['questions'].append(sets)
                 
-        return final_output,char_to_int,int_to_char
+        return final_output,char_to_int,int_to_char,len(l)
     
 def get_word_embeddings(input,hidden_size):
         embeds=gs.Word2Vec(input,min_count=1,size=hidden_size)
@@ -147,3 +163,44 @@ def get_question_set(input):
                         l.append(question)
                         
         return l
+    
+def get_prediction(start,end):
+        start=F.softmax(start,dim=1)
+        end=F.softmax(end,dim=1)
+        
+        start=start.squeeze(0)
+        end=end.squeeze(0)
+        
+        ma=0
+        start_pred=0
+        end_pred=0
+        
+        for i in range(start.shape[0]):
+                for j in range(i+1,end.shape[0]):
+                        if int(start[i])*int(end[j])>ma:
+                            
+                                ma=int(start[i])*int(end[j])
+                                start_pred=i
+                                end_pred=j
+                        
+        return [start_pred,end_pred]
+    
+def get_f1(gold,pred):
+        if gold[0]>pred[1] or gold[1]<pred[0]:
+                return 0
+        
+        final=gold+pred
+        final.sort()
+        
+        match=final[2]-final[1]+1
+        precision=match/(pred[1]-pred[0]+1)
+        recall=match/(gold[1]-gold[0]+1)
+        
+        f1=2*precision*recall(precision+recall)
+        f1*=100
+        
+        return f1
+    
+def get_exact_match(gold,pred):
+        
+        return 100*(gold[0]==pred[0] and gold[1]==pred[1])
